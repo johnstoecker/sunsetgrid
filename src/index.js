55ec3ef2a41fb115a37077580583057fe814301a
John Stoecker <johnstoecker@gmail.com>
2019-01-31 21:11:14 -0800

    cleanup shaders from index

--- src/index.js
+++ src/index.js
@@ -78,13 +78,19 @@ var uniforms = {
   },
 };
 
-var fShader = document.getElementById('fragmentShader').text;
-var vShader = document.getElementById('vertexShader').text;
-
 var material = new THREE.ShaderMaterial({
   uniforms: uniforms,
-  vertexShader: vShader,
-  fragmentShader: fShader
+  vertexShader: `varying vec2 vUv;
+  void main() {
+  vUv = uv;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position,1.0);
+  }`,
+  fragmentShader: `uniform vec3 color1;
+  uniform vec3 color2;
+  varying vec2 vUv;
+  void main() {
+    gl_FragColor = vec4(mix(color1, color2, vUv.y),1.0);
+  }`
 });
 
 // add a setting sun
